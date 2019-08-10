911824344ca2649f4e76226ad1514ba17721b0d5
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-10 10:17:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+  
+  get '/square/:number' do
+    @number = params[:number].to_i 
+    Math.sqrt(@number).to_s 
+  end
 
 end
