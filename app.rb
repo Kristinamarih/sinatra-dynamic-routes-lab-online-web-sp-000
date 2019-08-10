22472621532f7e7dd50e1cda885f260f2abf7908
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-10 10:20:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   
   get '/square/:number' do
     @number = params[:number].to_i 
-    Math.sqrt(@number)
+    @square_number = Math.sqrt(@number)
   end
   
   get '/say/:number/:phrase' do
